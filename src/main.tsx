be00c41359246dcbbe2d3f0c755d478eb71549bb
Md.Raihan Uddin <raihanarif778@gmail.com>
2023-07-14 23:43:14 +0600

    Footer Component addedd

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { RouterProvider } from 'react-router-dom';
 
 import './index.css';
 import { store } from './redux/store';
+import routes from './routes/routes';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
