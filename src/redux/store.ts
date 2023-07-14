634de1749420214f2d8005bd135f4677d6eb6ccc
Md.Raihan Uddin <raihanarif778@gmail.com>
2023-07-15 00:26:04 +0600

    api reducer with middleware added

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import UserReducer from './features/user/userSlice';
+import { api } from './api/apiSlice';
 
 export const store = configureStore({
   reducer: {
     user: UserReducer,
+    [api.reducerPath]: api.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(api.middleware),
